5a47ddbcb199cbc3cc110381e457d143912275cc
SantiagosWorkplace <santiagolucero00@gmail.com>
2022-11-20 22:08:06 -0300

    borre unas fotos y ecualicé un poco la pagina

--- funciones.js
+++ funciones.js
@@ -7,7 +7,7 @@ window.onload = function () {
         './img/5.jpeg',
         './img/6.png',
     ];
-    const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
+    const TIEMPO_INTERVALO_MILESIMAS_SEG = 3000;
     let posicionActual = 0;
     let $botonRetroceder = document.querySelector('#retroceder');
     let $botonAvanzar = document.querySelector('#avanzar');
